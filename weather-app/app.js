ca903f1f2270f766af70d7d608f502d8c2e9e1d1
Robin Kurtz <robin.kurtz@osedea.com>
2020-07-28 16:57:13 -0400

    Section 6 [...]

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,40 @@
-console.log('Starting');
+const request = require("request");
 
-setTimeout(() => {
-    console.log('2 Second Timer');
-}, 2000);
+const WEATHERSTACK_ACCESS_KEY = 'e0e09c23a68dda1f9f0a8cfda79611c3';
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoicm9iaW5rdXJ0em9zIiwiYSI6ImNrZDZla3VrZTJnaWwyc215MjYwYm1kaWgifQ.8GmR3COVCVfocIvf8e7SRA';
 
-setTimeout(() => {
-    console.log('0 Second Timer');
-}, 0);
+const geoCodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${MAPBOX_ACCESS_TOKEN}&limit=1`;
 
-console.log('Stopping');
+const getWeatherAPIUrl = (lat, long) => `http://api.weatherstack.com/current?access_key=${WEATHERSTACK_ACCESS_KEY}&query=${lat},${long}`;
+
+request(
+  {
+    url: getWeatherAPIUrl(),
+    json: true,
+  },
+  (error, response) => {
+    const current = response.body.current;
+    if (!response.body.current) {
+        console.log('No current data provided');
+    } else {
+        console.log(
+            `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`
+        );
+    }
+  }
+);
+
+request(
+    {
+      url: geoCodingUrl,
+      json: true,
+    },
+    (error, response) => {
+      const current = response.body;
+      console.log(current.features[0].center[1], current.features[0].center[0]);
+      console.log(getWeatherAPIUrl(
+        current.features[0].center[1],
+        current.features[0].center[0]
+      ))
+    }
+  );
