94fef547ac96f43c546a12ee14c201ee295d06b9
Robin Kurtz <robin.kurtz@osedea.com>
2020-08-14 14:37:30 -0400

    Section 15

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -69,7 +69,7 @@ userSchema.virtual('tasks', {
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this;
-    const token = jwt.sign({ _id: user._id.toString() }, 'derp', {
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {
         expiresIn: '7 days',
     });
 
